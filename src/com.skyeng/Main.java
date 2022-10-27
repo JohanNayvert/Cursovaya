88c6fdcdec2dcfe90f78947f803938842e6561ab
JohanNayvert <111690307+JohanNayvert@users.noreply.github.com>
2022-10-28 03:59:29 +0500

    First Commit

---
+++ src/com.skyeng/Main.java
@@ -0,0 +1,10 @@
+package com.skyeng;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Cursovaya_1");
+        System.out.println();
+
+        System.out.println("HomeWork Complete");
+    }
+}
